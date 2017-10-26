ef565816d7000d06d17db4460825a00db3f34296
mxsz <249812347@qq.com>
2017-10-26 14:00:01 +0800

    Add files via upload

---
+++ music/initList.php
@@ -0,0 +1,36 @@
+<?php
+	echo '[
+			{"id":"1","name":"张杰","musicName":"逆战"},
+			{"id":"2","name":"张学友","musicName":"烦恼歌"},
+			{"id":"3","name":"周杰伦","musicName":"红尘客栈"},
+			{"id":"4","name":"李建","musicName":"异乡人"},
+			{"id":"5","name":"陈小春","musicName":"相依为命"},
+			{"id":"6","name":"李荣浩","musicName":"不将就"},
+			{"id":"7","name":"郝云","musicName":"去大理"},
+			{"id":"8","name":"宋冬野","musicName":"董小姐"},
+			{"id":"9","name":"许巍","musicName":"曾经的你"},
+			{"id":"10","name":"朴树","musicName":"那些花儿"},
+			
+			{"id":"1","name":"张杰","musicName":"逆战"},
+			{"id":"2","name":"张学友","musicName":"烦恼歌"},
+			{"id":"3","name":"周杰伦","musicName":"红尘客栈"},
+			{"id":"4","name":"李建","musicName":"异乡人"},
+			{"id":"5","name":"陈小春","musicName":"相依为命"},
+			{"id":"6","name":"李荣浩","musicName":"不将就"},
+			{"id":"7","name":"郝云","musicName":"去大理"},
+			{"id":"8","name":"宋冬野","musicName":"董小姐"},
+			{"id":"9","name":"许巍","musicName":"曾经的你"},
+			{"id":"10","name":"朴树","musicName":"那些花儿"},
+			
+			{"id":"1","name":"张杰","musicName":"逆战"},
+			{"id":"2","name":"张学友","musicName":"烦恼歌"},
+			{"id":"3","name":"周杰伦","musicName":"红尘客栈"},
+			{"id":"4","name":"李建","musicName":"异乡人"},
+			{"id":"5","name":"陈小春","musicName":"相依为命"},
+			{"id":"6","name":"李荣浩","musicName":"不将就"},
+			{"id":"7","name":"郝云","musicName":"去大理"},
+			{"id":"8","name":"宋冬野","musicName":"董小姐"},
+			{"id":"9","name":"许巍","musicName":"曾经的你"},
+			{"id":"10","name":"朴树","musicName":"那些花儿"}
+		]';
+?>
